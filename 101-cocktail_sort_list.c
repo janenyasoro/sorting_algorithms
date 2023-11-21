293636e9b565a871f5e231fd608ce72133692423
janenyasoro <janenyasoro@gmail.com>
2023-11-21 03:01:08 -0800

    A function that sorts an array of integers in ascending order using the Counting sort algorithm

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,19 @@
+#inclde "sort.h"
+
+void swap_node_ahead(listint_t **list, listint_t **tail,listint_t **shaker);
+void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker);
+void cocktail_sort_list(listint_t **list);
+
+#inclde "sort.h"
+
+void swap_node_ahead(listint_t **list, listint_t **tail,listint_t **shaker);
+void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker);
+void cocktail_sort_list(listint_t **list);
+
+#inclde "sort.h"
+
+void swap_node_ahead(listint_t **list, listint_t **tail,listint_t **shaker);
+void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker);
+void cocktail_sort_list(listint_t **list);
+
+
